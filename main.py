4220ce5dd9271a3790ffa471d8406b94cf94d1db
markcompsci <84529187+markcompsci@users.noreply.github.com>
2021-06-14 17:40:45 +0000

    more linter errors

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ result = ""
 # It splits the integer from the float, cuts off excess digits from the float,
 # then, concatenates them together
 
+
 def fixed(num):
     num = str(num)
     integer = num.split(".")[0]
     decimal = num.split(".")[1]
     return float(f"{integer}.{decimal[:3]}")
+
 
 result += f"The area is {fixed(PI * (RADIUS ** 2))}cm\n\n"
 result += f"The circumference is {fixed(2 * PI * RADIUS)}cm"
